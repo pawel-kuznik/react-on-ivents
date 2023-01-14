dfbb2baed8ad2512546b6e4d2ee2541eba6a6feb
pawel-kuznik <pawel.kuznik@gmail.comm>
2023-01-14 20:16:27 +0100

    Wrapped up Reader class.

--- src/Reader.test.ts
+++ src/Reader.test.ts
@@ -2,12 +2,66 @@ import { Reader } from "./Reader";
 
 describe('Reader', () => {
 
-    it('should load data', () => {
+    it('should load data', async () => {
 
         const reader = new Reader(() => Promise.resolve(42));
-        reader.start();
+        await reader.start();
         const result = reader.read();
 
         expect(result).toEqual(42);
+    });
+    it('should throw when the data is not yet there', done => {
+
+        try {
+            const reader = new Reader(() => Promise.resolve(42));
+            reader.start();
+            reader.read();
+        }
+
+        catch(reason: any) {
+
+            expect('then' in reason).toBeTruthy();
+            done();
+        }
+    });
+    it('should throw when read without starting (cause it needs to ask for load)', done => {
+
+        try {
+            const reader = new Reader(() => Promise.resolve(42));
+            reader.read();
+        }
+
+        catch(reason: any) {
+
+            expect('then' in reason).toBeTruthy();
+            done();
+        }
+    });
+    it('should honor the parameter', async () => {
+
+        const reader = new Reader<number, number>((param: number) => Promise.resolve(param));
+        const startResult = await reader.start(42);
+        expect(startResult).toEqual(42);
+        expect(reader.result).toEqual(42);
+        expect(reader.read(42)).toEqual(42);
+    });
+    it('should assign error when promise rejects', async () => {
+
+        const error = Error("good");
+        const reader = new Reader<void>(() => Promise.reject(error));
+        await reader.start();
+        expect(reader.error).toEqual(error);
+    });
+    it('should not reload when the parameter does not change', async () => {
+
+        const reader = new Reader<number, number>((param: number) => Promise.resolve(param));
+        await reader.start(42);
+        expect(() => reader.read(42)).not.toThrow();
+    });
+    it('should reload when the parameter changes', async () => {
+
+        const reader = new Reader<number, number>((param: number) => Promise.resolve(param));
+        await reader.start(42);
+        expect(() => reader.read(16)).toThrow();
     });
 });
--- src/Reader.ts
+++ src/Reader.ts
@@ -1,6 +1,27 @@
-import { AbortablePromise } from "./AbortablePromise";
+import { AbortablePromise } from "@pawel-kuznik/blindsight";
 
 export type ReaderFetch<TReturn, TParam> = (param: TParam) => Promise<TReturn>;
+
+
+export interface ReaderOptions<TParam> {
+
+    /**
+     *  A function that allows to compare two parameters when the read happens.
+     *  By default the Reader will use `Object.is()`, as it's a also the default
+     *  way to compare parameters in React.
+     * 
+     *  true - means both params are considered equal.
+     *  false - means params are different.
+     */
+    paramsComparator?: (paramA: TParam, paramB: TParam) => boolean;
+};
+
+/**
+ *  An internal reader settings that require all options.
+ */
+interface ReaderSettings<TParam> {
+    paramsComparator: (paramA: TParam, paramB: TParam) => boolean;
+};
 
 /**
  *  A Reader is a special object that allows reading from an async source
@@ -15,9 +36,21 @@ export class Reader<TReturn, TParam = void> {
     private _param: TParam | undefined = undefined;
     private _error: any;
 
+    private _settings: ReaderSettings<TParam>;
+
+    get result() : TReturn|undefined { return this._result; }
     get error() : any { return this._error; }
 
-    constructor(fetch: ReaderFetch<TReturn, TParam>) {
+    constructor(fetch: ReaderFetch<TReturn, TParam>, options: ReaderOptions<TParam> = { }) {
+
+        this._settings = {
+            // set default settings
+            paramsComparator: Object.is,
+
+            // and override some of the settings with options
+            ...options
+        };
+
         this._fetch = fetch;
     }
 
@@ -26,28 +59,35 @@ export class Reader<TReturn, TParam = void> {
      */
     read(param: TParam) : TReturn {
 
-        if (this._result === undefined) {
+        this.start(param);
 
-            this._promise = this.start(param);
-            this._promise.then(result => {
-                this._result = result;
-            }, (reason: any) => {
-                this._error = reason;
-            });
-
-            throw this._promise;
-        }
+        if (this._result === undefined) throw this._promise;
 
         return this._result;
     }
 
     /**
-     *  Start reading the data.
+     *  Start reading the data. The method returns a promise that resolves
+     *  when the reading process is done (either resolves or rejects).
      */
-    start(param: TParam) : AbortablePromise<TReturn> {
+    start(param: TParam) : Promise<TReturn|undefined> {
 
-        if (!this._promise) this._promise = new AbortablePromise(this._fetch(param));
-        return this._promise;
+        if (!this._promise || (param !== this._param && this._param === undefined) || (this._param !== undefined && !this._settings.paramsComparator(param, this._param))) {
+
+            // if we need to redo the read we need to reset the whole object as we will have different data.
+            this.reset();
+
+            this._param = param;
+
+            this._promise = new AbortablePromise(this._fetch(param));
+            this._promise.then(result => {
+                this._result = result;
+            }, reason => {
+                this._error = reason;
+            });
+        }
+
+        return this._promise.then(undefined, () => { return undefined; });
     }
 
     /**
@@ -59,12 +99,14 @@ export class Reader<TReturn, TParam = void> {
         this._promise = undefined;
         this._error = undefined;
         this._param = undefined;
+        this._result = undefined;
     }
 
     /**
-     *  Reload the reader.
+     *  Reload the reader. Similar to `.start()` method, this method returns a promise
+     *  that resolves when the reload process is done (either resolves or rejects);
      */
-    reload(param: TParam) : Promise<TReturn> {
+    reload(param: TParam) : Promise<TReturn|undefined> {
 
         this.reset();
         return this.start(param);
